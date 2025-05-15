7b5fed075926addaba1eb63cbc2b7c4dad019f4d
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-05-15 08:46:19 -0300

    fix: corrige estrutura padrão MVC

---
+++ src/main/java/br/edu/uepb/restaurante/App.java
@@ -0,0 +1,10 @@
+package br.edu.uepb.restaurante;
+
+import javafx.application.Application;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module br.edu.uepb.sistemarestaurante.sistemarestaurante {
+module br.edu.uepb.restaurante.sistemarestaurante {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens br.edu.uepb.sistemarestaurante to javafx.fxml;
-    exports br.edu.uepb.sistemarestaurante;
+    opens br.edu.uepb.restaurante to javafx.fxml;
+    exports br.edu.uepb.restaurante;
 }
